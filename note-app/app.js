045483af80299bffa42a20ef8dca7cb523995595
Kyle Hall <hallkd47@gmail.com>
2018-01-18 20:15:24 -0600

    adds beginning of the cli

--- note-app/app.js
+++ note-app/app.js
@@ -1,12 +1,21 @@
 
+const fs = require('fs');
 const _ = require('lodash');
+
 const notes = require('./notes');
 
-console.log(notes.add(1, 3));
+let command = process.argv[2];
 
-console.log(_.isString(true));
-console.log(_.isString('Andrew'));
+console.log(process.argv);
 
-const arr = [1, 3, 5, 6, 6, 7, 1, 5, 3];
-
-console.log(_.uniq(arr));
+if (command === 'add') {
+    console.log("Adding a new note...");
+} else if (command === 'list') {
+    console.log("Listing all the notes...");
+} else if (command === 'read') {
+    console.log('Reading note...');
+} else if (command === 'remove') {
+    console.log('Removing the note...');
+} else {
+    console.log('Command not recognized. Try again..');
+}
