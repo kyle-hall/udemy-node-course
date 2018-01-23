5789add5a81571e101a51f401aea5d2c72ac8d2c
Kyle Hall <hallkd47@gmail.com>
2018-01-22 21:51:15 -0600

    fixes eslint errors

--- note-app/app.js
+++ note-app/app.js
@@ -1,6 +1,4 @@
 
-const fs = require('fs');
-const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
@@ -10,26 +8,26 @@ const argv = yargs.argv;
 const command = process.argv[2];
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    const allNotes = notes.getAll();
-    console.log(allNotes);
+  const allNotes = notes.getAll();
+  console.log(allNotes);
 } else if (command === 'read') {
-    if (!argv.title) {
-        console.log("Read needs a title. Try using '--title'");
-        process.exit(1);
-    }
+  if (!argv.title) {
+    console.log('Read needs a title. Try using --title');
+    process.exit(1);
+  }
 
-    const note = notes.readNote(argv.title);
-    console.log(note);
+  const note = notes.readNote(argv.title);
+  console.log(note);
 } else if (command === 'remove') {
-    if (!argv.title) {
-        console.log("Remove needs a title. Try using '--title'");
-        process.exit(1);
-    }
+  if (!argv.title) {
+    console.log('Remove needs a title. Try using --title');
+    process.exit(1);
+  }
 
-    notes.removeNote(argv.title);
-    console.log('Removing the note...');
+  notes.removeNote(argv.title);
+  console.log('Removing the note...');
 } else {
-    console.log('Command not recognized. Try again..');
+  console.log('Command not recognized. Try again..');
 }
--- note-app/notes.js
+++ note-app/notes.js
@@ -1,7 +1,7 @@
 
 
 const addNote = (title, body) => {
-    console.log('Adding note', title, body);
+  console.log('Adding note', title, body);
 };
 
 const getAll = () => "Getting all notes.";
@@ -11,8 +11,8 @@ const readNote = title => "Here's your note.";
 const removeNote = title => "Removed your note.";
 
 module.exports = {
-    addNote,
-    getAll,
-    readNote,
-    removeNote
+  addNote,
+  getAll,
+  readNote,
+  removeNote
 };
