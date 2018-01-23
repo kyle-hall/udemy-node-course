a26546352f3c10befd3084c0bdce557f65b23dff
Kyle Hall <hallkd47@gmail.com>
2018-01-22 22:24:15 -0600

    saves notes into JSON file

--- note-app/notes.js
+++ note-app/notes.js
@@ -1,14 +1,38 @@
 
+const fs = require('fs');
 
 const addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  let notes = [];
+
+  try {
+    notes = JSON.parse(fs.readFileSync('note-data.json'));
+  } catch (e) {
+    console.error('Error: ', e);
+  }
+
+  const hasDuplicates = notes.find(e => e.title === title);
+
+  if (hasDuplicates) {
+    console.log(`Found a duplicate for ${title}`);
+    return;
+  }
+
+  const note = {
+    title,
+    body
+  };
+
+  notes.push(note);
+  fs.writeFile('note-data.json', JSON.stringify(notes), err => {
+    if (err) throw err;
+  });
 };
 
-const getAll = () => "Getting all notes.";
+const getAll = () => 'Getting all notes.';
 
-const readNote = title => "Here's your note.";
+const readNote = title => 'Here\'s your note.';
 
-const removeNote = title => "Removed your note.";
+const removeNote = title => 'Removed your note.';
 
 module.exports = {
   addNote,
