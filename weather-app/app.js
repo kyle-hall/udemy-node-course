030bc6f6f07359655678cae8776c9472af873d49
Kyle Hall <hallkd47@gmail.com>
2018-01-28 22:47:49 -0600

    adds call to darksky client, wraps execution in main method

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,43 @@
 
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const darksky = require('./clients/darksky-client');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Street address',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+const main = () => {
 
-geocode.geocode(argv.address, (errorMessage, results) => {
+  const argv = yargs
+    .options({
+      a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Street address',
+        string: true
+      }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+  geocode.geocode(argv.address, geocodeCallback);
+
+};
+
+const darkskyCallback = (err, result) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log('Printing the data', result.currently.temperature);
+};
+
+const geocodeCallback = (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
     process.exit(1);
   }
 
-  console.log(JSON.stringify(results, undefined, 2));
-});
+  darksky.sendRequest(results.latitude, results.longitude, darkskyCallback);
+};
+
+main();
