6f2dd93ea57a14c6f8449a88a7b6078b6fc023ec
Kyle Hall <hallkd47@gmail.com>
2018-01-28 20:37:36 -0600

    encoding user-entered address for the geolocation request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,30 @@
 
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Street address',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const address = encodeURIComponent(argv.address);
 
 const options = {
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4308+Wakonda+Parkway+des+moines',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 };
 
 const callback = (err, response, body) => {
   const data = body.results[0];
+  console.log(data.formatted_address);
   console.log(data.geometry);
 };
 
